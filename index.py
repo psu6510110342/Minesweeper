8e3cbfc3f9ff63f5566c495ad717413c08810f32
phattarakorn <phattarakorn23228@gmail.com>
2023-02-06 12:02:17 +0700

    upgrade grid

--- index.py
+++ index.py
@@ -7,17 +7,10 @@ from kivy.uix.button import Button
 
 class MyApp(App):
     def build(self):
-        layout = GridLayout(cols=2)
-    
-        btn1 = Button(text='Hello 1')
-        btn2 = Button(text='Hello 2')
-        btn3 = Button(text='Hello 3')
-        btn4 = Button(text='Hello 4')
+        layout = GridLayout(cols=8)
 
-        layout.add_widget(btn1)
-        layout.add_widget(btn2)
-        layout.add_widget(btn3)
-        layout.add_widget(btn4)
+        for i in range(1,65):
+            layout.add_widget(Button(text='test'))
 
         return layout
         
